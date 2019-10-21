42b0c6a7fa9f09432e202990a58527f3fd46a1c1
Kyle Bocinsky <bocinsky@gmail.com>
2019-10-21 08:58:29 -0700

    first draft October 2019

--- docs/mtdrought-2019-october/R/setup.R
+++ docs/mtdrought-2019-october/R/setup.R
@@ -1,6 +1,6 @@
 ## Load all packages
 all_packages <- c("mcor", "thredds",# The Core MCO package
-                  "FedData", "smapr", "rhdf5", # Package for data aquisition
+                  "FedData", "smapr", # Package for data aquisition
                   "sf", "raster", "rgeos", "fasterize", "velox", "spex", "lwgeom", "geojsonsf", # Packages for spatial processing
                   "magrittr", "tidyverse", "purrrlyr", "matrixStats",# Packages for tidy code
                   "reticulate", # Python in R
